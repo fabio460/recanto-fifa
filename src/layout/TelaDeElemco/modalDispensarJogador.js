d8a12570d80db17404269366197341505621f695
fabio dasilva de oliveira <dasilvadeoliveirafabio@gmail.com>
2023-03-06 09:57:50 -0300

    transferencia remota teste 4

---
+++ src/layout/TelaDeElemco/modalDispensarJogador.js
@@ -0,0 +1,110 @@
+import React,{useState,useEffect} from 'react';
+import Button from '@mui/material/Button';
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+import DialogTitle from '@mui/material/DialogTitle';
+import Box from '@mui/material/Box';
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import Select from '@mui/material/Select';
+import TextField from '@mui/material/TextField';
+import { useDispatch } from 'react-redux/es/exports';
+import AutoComplete from '../TelaListaDeJogadores/AutoComplete';
+import { lista } from '../../Lista'
+import { criarUsuarioApi, deletarUsuarioApi, listaDeUsuariosApi } from '../../api';
+import { Autocomplete } from '@mui/material';
+export default function ModalDispensarJogador() {
+    const [usuarios, setUsuarios] = useState([])
+    const [value, setValue] = React.useState(null);  
+    const [open, setOpen] = React.useState(false);
+    const [nome, setNome] = useState()
+    const [folha, setFolha] = useState(0)
+    const [saldo, setSaldo] = useState(0)
+    const handleClickOpen = () => {
+        setOpen(true);
+    };
+
+    const dispatch = useDispatch()
+    const handleClose = () => {
+        setOpen(false);
+    };
+
+    const dialogStyle = {
+        width:"600px",
+        height:"",
+        padding:"20px",
+        "@media (max-width:800px)":{
+        width:"100%"
+        }
+    }
+    const Confirmar = ()=>{
+         deletarUsuarioApi(age)
+        handleClose()
+    }
+
+    async function getUsuarios() {
+        const p =await listaDeUsuariosApi()
+        setUsuarios(p)
+       
+    }
+    useEffect(()=>{
+        getUsuarios()
+      },[value])
+
+    const [age, setAge] = React.useState('');
+
+    const handleChange = (event) => {
+        setAge(event.target.value);
+    };  
+      
+  return (
+    <div>
+      <button onClick={handleClickOpen} className='btn btn-primary'>Despensar</button>           
+      <Dialog
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="alert-dialog-title"
+        aria-describedby="alert-dialog-description"
+        sx={{background:"",width:"100%"}}
+      >
+        <DialogTitle id="alert-dialog-title">
+           Deseja realmente dispensar o jogador?
+        </DialogTitle>
+        <DialogContent sx={dialogStyle}>
+          <DialogContentText 
+            id="alert-dialog-description" 
+            sx={{display:"flex",flexDirection:"column"}}
+            >
+               <div style={{marginTop:"12px"}}>
+                    <Box sx={{ minWidth: 100 }}>
+                        <FormControl fullWidth>
+                            <InputLabel id="demo-simple-select-label">Usuarios</InputLabel>
+                            <Select
+                            labelId="demo-simple-select-label"
+                            id="demo-simple-select"
+                            value={age}
+                            label="Usuarios"
+                            onChange={handleChange}
+                            >
+                                { usuarios?.map((e,key)=>{
+                                    return <MenuItem value={e.id}>{e.nome}</MenuItem>
+                                })}
+                            </Select>
+                        </FormControl>
+                    </Box>
+                </div>
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={Confirmar}>Confirmar</Button>
+          <Button onClick={handleClose} autoFocus>
+            Cancelar
+          </Button>
+        </DialogActions>
+      </Dialog>
+    </div>
+  );
+}
---
+++ src/layout/TelaDeElemco/modalTransferirJogador.js
@@ -0,0 +1,118 @@
+import React,{useState,useEffect} from 'react';
+import Button from '@mui/material/Button';
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+import DialogTitle from '@mui/material/DialogTitle';
+import Box from '@mui/material/Box';
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import Select from '@mui/material/Select';
+import TextField from '@mui/material/TextField';
+import { useDispatch } from 'react-redux/es/exports';
+import AutoComplete from '../TelaListaDeJogadores/AutoComplete';
+import { lista } from '../../Lista'
+import { criarUsuarioApi, deletarUsuarioApi, listaDeUsuariosApi, transferenciaDeJogadorApi } from '../../api';
+import { Autocomplete } from '@mui/material';
+export default function ModalTransferirJogador({id}) {
+    const [usuarios, setUsuarios] = useState([])
+    const [value, setValue] = React.useState(null);  
+    const [open, setOpen] = React.useState(false);
+    const [valor, setValor] = useState()
+    const handleClickOpen = () => {
+        setOpen(true);
+    };
+
+    const dispatch = useDispatch()
+    const handleClose = () => {
+        setOpen(false);
+    };
+
+    const dialogStyle = {
+        width:"600px",
+        height:"",
+        padding:"20px",
+        "@media (max-width:800px)":{
+        width:"100%"
+        }
+    }
+    const Confirmar = ()=>{
+         //deletarUsuarioApi(age)
+        //handleClose()
+        let idUsuario = age
+       
+        transferenciaDeJogadorApi(id, idUsuario, parseFloat(valor))
+    }
+
+    async function getUsuarios() {
+        const p =await listaDeUsuariosApi()
+        setUsuarios(p)
+       
+    }
+    useEffect(()=>{
+        getUsuarios()
+        console.log(value)
+      },[value])
+
+    const [age, setAge] = React.useState('');
+
+    const handleChange = (event) => {
+        setAge(event.target.value);
+    };  
+      
+  return (
+    <div>
+      <button onClick={handleClickOpen} className='btn btn-success ms-3'>Transferir</button>           
+      <Dialog
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="alert-dialog-title"
+        aria-describedby="alert-dialog-description"
+        sx={{background:"",width:"100%"}}
+      >
+        <DialogTitle id="alert-dialog-title">
+           Deseja realmente transferir ?
+           
+        </DialogTitle>
+        <DialogContent sx={dialogStyle}>
+          <DialogContentText 
+            id="alert-dialog-description" 
+            sx={{display:"flex",flexDirection:"column"}}
+            >
+               <div style={{}}>
+                
+                    <Box sx={{ minWidth: 100, minHeight:180 }}>
+                        <FormControl fullWidth sx={{margin:"10px 0px"}}>
+                            <InputLabel id="demo-simple-select-label">Transferir para</InputLabel>
+                            <Select
+                              sx={{}}
+                              labelId="demo-simple-select-label"
+                              id="demo-simple-select"
+                              value={age}
+                              size="small"
+                              label="Transferir para"
+                              onChange={handleChange}
+                            >
+                                { usuarios?.map((e,key)=>{
+                                    return <MenuItem value={e.id}>{e.nome}</MenuItem>
+                                })}
+                            </Select>
+                        </FormControl>
+                        <TextField onChange={e=>setValor(e.target.value)} label="Valor da transferência" size='small' sx={{marginTop:"12px", width:"100%"}}/>
+
+                    </Box>
+                </div>
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={Confirmar}>Confirmar</Button>
+          <Button onClick={handleClose} autoFocus>
+            Cancelar
+          </Button>
+        </DialogActions>
+      </Dialog>
+    </div>
+  );
+}
