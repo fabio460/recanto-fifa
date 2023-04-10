9f912350092d2ad2c8ff388e3dfad7c778752e38
fabio dasilva de oliveira <dasilvadeoliveirafabio@gmail.com>
2023-04-10 19:57:12 -0300

    ajuste nos textos de erro

--- src/layout/Usuarios/index.js
+++ src/layout/Usuarios/index.js
@@ -4,13 +4,15 @@ export default function Usuarios({Lista}) {
   
 
   return (
-    <div className='UsuariosContainer'>
-        <h2 style={{textAlign:'center', marginTop:20}}>Usuários</h2>
-        {Lista?.map((elem,key)=>{
-            return (
-               <CardUsuarios usuario={elem}/>
-            )
-        })}
+    <div>
+      <h2 style={{textAlign:'center', marginTop:20}}>Usuários</h2>
+      <div className='UsuariosContainer'>
+          {Lista?.map((elem,key)=>{
+              return (
+                <CardUsuarios usuario={elem}/>
+              )
+          })}
+      </div>
     </div>
   )
 }
